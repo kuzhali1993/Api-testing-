dcb7976183d93a73f90391a32b237e707271bdc3
Kuzhali Maheswaran <kuzhali.maheswaran@kovai.co>
2020-04-30 16:41:49 +0530

    commit the first changes

---
+++ APIFramework_Document360/APIFramework_Document360/Testcases/sample.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIFramework_Document360.Testcases
+{
+    class sample
+    {
+        [Test]
+        public void sampletest()
+        {
+            Console.WriteLine("kuzhali");
+        }
+    }
+}
